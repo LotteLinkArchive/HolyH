33f7b4443bf2af0204c1923c3a6a820c33c36db2
Naphtha Nepanthez <naphtha@lotte.link>
2020-09-20 03:22:24 +0100

    Fix tabs

--- src/types.h
+++ src/types.h
@@ -7,9 +7,9 @@
 #include <stdbool.h> // C99
 
 // Integer maximums
-#define U8_MAX	UINT8_MAX
-#define I8_MIN	INT8_MIN
-#define I8_MAX	INT8_MAX
+#define U8_MAX  UINT8_MAX
+#define I8_MIN  INT8_MIN
+#define I8_MAX  INT8_MAX
 #define U16_MAX UINT16_MAX
 #define I16_MIN INT16_MIN
 #define I16_MAX INT16_MAX
@@ -24,14 +24,14 @@
 typedef	void X0;
 
 // Integer typedefs
-typedef uint8_t	U8;
-typedef	int8_t	I8;
+typedef uint8_t  U8;
+typedef	int8_t   I8;
 typedef uint16_t U16;
-typedef	int16_t I16;
+typedef	int16_t  I16;
 typedef uint32_t U32;
-typedef	int32_t I32;
+typedef	int32_t  I32;
 typedef uint64_t U64;
-typedef	int64_t I64;
+typedef	int64_t  I64;
 
 // String component types
 // Only use these for storing characters for strings.
@@ -47,7 +47,7 @@ typedef	U32 UNAT;
 typedef	R32 RNAT;
 
 // Size type
-typedef	size_t SX;	// Size type, length unspecified
+typedef	size_t SX; // Size type, length unspecified
 typedef	ssize_t SSX; // Size type, length unspecified, and signed
 
 // Boolean types
