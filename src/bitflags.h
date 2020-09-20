ee3826cec84c6685234a42020c2bf8cde0705ce8
Naphtha Nepanthez <naphtha@lotte.link>
2020-09-20 17:48:13 +0100

    Added HLYRGEXTRCT

--- src/bitflags.h
+++ src/bitflags.h
@@ -2,6 +2,7 @@
 #define HOLYBFLG_HEADER_FILE
 
 #include "types.h"
+#include "macros.h"
 
 #define HLYCHCKFLG(field, bit) \
 	(BOOLIFY((field) & ((__typeof__ (field))0x01 << (bit))))
@@ -11,5 +12,8 @@
 	((field) & ~((__typeof__ (field))0x01 << (bit)))
 #define HLYTOGLFLG(field, bit) \
 	((field) ^ ((__typeof__ (field))0x01 << (bit)))
+
+#define HLYRGEXTRCT(source, bits, position) \
+	(source >> position & (U16Pow(2, bits) - 1))
 
 #endif
